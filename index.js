45557ea43a16fd9a08834d476b218764aa149502
Win Nguyen <140900453+winnguyen1905@users.noreply.github.com>
2025-06-24 23:23:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ export default async ({ req, res, log }) => {
         const { to_zip, to_state, amount, shipping = 0 } = JSON.parse(req.body);
 
         if (!to_zip || !to_state || !amount) {
-            log("Message: ", "Missing required parameters.");
+            log("1: ", "Missing required parameters.");
             return res.send({
                 error: 'Missing required parameters.',
             });
@@ -16,7 +16,7 @@ export default async ({ req, res, log }) => {
         const client = new Taxjar({ apiKey: process.env.TAXJAR_API_KEY });
 
         if (!client) {
-            log("Message: ", "Taxjar client not initialized.");
+            log("2: ", "Taxjar client not initialized.");
             return res.send({
                 error: 'Taxjar client not initialized.',
             });
@@ -36,7 +36,7 @@ export default async ({ req, res, log }) => {
 
         // Check if response contains the expected 'tax' object
         if (response && response.tax) {
-            log("Message: ", "Taxjar response: ", response);
+            log("3: ", "Taxjar response: ", response);
             const { tax } = response;
 
             // Send response as JSON
@@ -46,14 +46,14 @@ export default async ({ req, res, log }) => {
             });
 
         } else {
-            log("Message: ", "Invalid response from Taxjar API. No tax information found.");
+            log("4: ", "Invalid response from Taxjar API. No tax information found.");
             return res.send({
                 error: 'Invalid response from Taxjar API. No tax information found.',
             });
         }
 
     } catch (err) {
-        log("Error: ", err);
+        log("5: ", err);
         // Catch any errors and return them as a JSON response
         return res.send({
             error: err.message,  // Just send the error message, no need for the client object
