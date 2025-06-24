1b0a0286befda126ed877e0d24e858049efdb57d
Win Nguyen <winnguyen1905.work@gmail.com>
2025-06-24 22:36:28 +0700

    try catch

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 import Taxjar from 'taxjar';
 
 export default async ({ req, res }) => {
-    const { to_zip, to_state, amount, shipping = 0 } = JSON.parse(req.body);
+    try {
+        const { to_zip, to_state, amount, shipping = 0 } = JSON.parse(req.body);
 
-    const client = new Taxjar({ apiKey: process.env.TAXJAR_API_KEY });
+        const client = new Taxjar({ apiKey: process.env.TAXJAR_API_KEY });
 
-    console.log(process.env.TAXJAR_API_KEY);
+        const { tax } = await client.taxForOrder({
+            from_country: 'US',
+            from_state: 'CA',   // <- your nexus
+            from_zip: '94111',
+            to_country: 'US',
+            to_zip,
+            to_state,
+            amount,
+            shipping,
+        });
 
-    const { tax } = await client.taxForOrder({
-        from_country: 'US',
-        from_state: 'CA',   // <- your nexus
-        from_zip: '94111',
-        to_country: 'US',
-        to_zip,
-        to_state,
-        amount,
-        shipping,
-    });
-
-    return res.json({
-        salesTax: tax.amount_to_collect,
-        rate: tax.rate,
-    });
+        return res.json({
+            salesTax: tax.amount_to_collect,
+            rate: tax.rate,
+        });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
 };
