3f235980c2e74e49d0505e2ad4eed28cfa203cc3
Win Nguyen <winnguyen1905.work@gmail.com>
2025-06-24 22:46:51 +0700

    fix status is not function

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 import Taxjar from 'taxjar';
 
 export default async ({ req, res }) => {
-    try {
-        const { to_zip, to_state, amount, shipping = 0 } = JSON.parse(req.body);
+  try {
+    // Parse the body of the request
+    const { to_zip, to_state, amount, shipping = 0 } = JSON.parse(req.body);
 
-        const client = new Taxjar({ apiKey: process.env.TAXJAR_API_KEY });
+    // Initialize Taxjar client
+    const client = new Taxjar({ apiKey: process.env.TAXJAR_API_KEY });
 
-        const { tax } = await client.taxForOrder({
-            from_country: 'US',
-            from_state: 'CA',   // <- your nexus
-            from_zip: '94111',
-            to_country: 'US',
-            to_zip,
-            to_state,
-            amount,
-            shipping,
-        });
+    // Calculate tax using Taxjar API
+    const { tax } = await client.taxForOrder({
+      from_country: 'US',
+      from_state: 'CA',   // Use your nexus state
+      from_zip: '94111',   // Use your nexus zip
+      to_country: 'US',
+      to_zip,
+      to_state,
+      amount,
+      shipping,
+    });
 
-        return res.json({
-            salesTax: tax.amount_to_collect,
-            rate: tax.rate,
-        });
-    } catch (error) {
-        return res.status(200).json({ error: error.message });
-    }
+    // Send response as JSON
+    res.send({
+      salesTax: tax.amount_to_collect,
+      rate: tax.rate,
+    });
+  } catch (error) {
+    // Catch any errors and return them as a JSON response
+    res.send({
+      error: error.message,
+    });
+  }
 };
