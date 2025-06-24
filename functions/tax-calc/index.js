acd100f8f9d986a5776bce871ed24b98b817a5cd
Win Nguyen <winnguyen1905.work@gmail.com>
2025-06-24 19:49:35 +0700

    taxjar function

---
+++ functions/tax-calc/index.js
@@ -0,0 +1,23 @@
+import Taxjar from 'taxjar';
+
+export default async ({ req, res }) => {
+    const { to_zip, to_state, amount, shipping = 0 } = JSON.parse(req.body);
+
+    const client = new Taxjar({ apiKey: process.env.TAXJAR_API_KEY });
+
+    const { tax } = await client.taxForOrder({
+        from_country: 'US',
+        from_state: 'CA',   // <- your nexus
+        from_zip: '94111',
+        to_country: 'US',
+        to_zip,
+        to_state,
+        amount,
+        shipping,
+    });
+
+    return res.json({
+        salesTax: tax.amount_to_collect,
+        rate: tax.rate,
+    });
+};
